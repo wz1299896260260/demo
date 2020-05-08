6b93fb228e0bafdc9545546a326ca4b97a3f2b13
wz1299896260 <1299896260@qq.com>
2020-05-08 14:34:31 +0800

    del

--- echo框架/web/control/control.go
+++
@@ -1,7 +0,0 @@
-package control
-
-import "github.com/labstack/echo"
-
-func Index(e echo.Context)error{
-	return e.Redirect(302,"/upload.html")//重定向
-}
--- echo框架/web/control/page.go
+++
@@ -1,9 +0,0 @@
-package control
-
-
-//分页结构体
-type Page struct {
-	Pi int `json:"pi"`
-	Ps int `json:"ps"`
-}
-
